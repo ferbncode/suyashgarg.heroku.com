f1a3f44486f3e031848e47838102631ec2ff49ac
ferbncode <suyashgargsfam@gmail.com>
2016-06-25 00:13:13 +0530

    projects.py

--- mysite/views/projects.py
+++ mysite/views/projects.py
@@ -1,8 +1,13 @@
 from flask import Blueprint, render_template
-
+from  ..models import *
 projects = Blueprint('projects', __name__)
 
 @projects.route('/')
 def index():
-	return render_template('projects/index.html')
+	projects_summary = []
+	#not feeling like adding generator...the below is slow and less memory-efficient
+	for project in query_db("select * from projects"):
+		projects_summary.append(project)
+	return render_template('projects/index.html', projects_summary=projects_summary)
+
 
