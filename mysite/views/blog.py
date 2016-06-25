e6e1cdfbb218b5c5b712e7569c9844345a04c67c
ferbncode <suyashgargsfam@gmail.com>
2016-06-25 12:54:25 +0530

    Completed backend for projects and started blogging part

--- mysite/views/blog.py
+++ mysite/views/blog.py
@@ -1,9 +1,19 @@
 from flask import Blueprint, render_template
-
+from  ..models import *
 blog = Blueprint('blog', __name__)
 
 @blog.route('/')
 def index():
-	return render_template('blog/index.html')
-
+	blog_summary = []
+	#not feeling like adding generator...the below is slow and less memory-efficient
+	for blog in query_db("select * from projects"):
+		blog_summary.append(blog)
+	return render_template('blog/index.html', blog_summary=blog_summary)
+@blog.route('/<blogname>')
+def projectread(blogname):
+	blog_detail = query_db("select * from projects where subject=?", [blogname])
+	blog_detail = project_detail[0]
+	if blog_detail == []:
+		return "Its a 404 buddy",404
+	return render_template("blog/blogdet.html", blog_detail = blog_detail)
 
--- mysite/views/projects.py
+++ mysite/views/projects.py
@@ -9,5 +9,11 @@ def index():
 	for project in query_db("select * from projects"):
 		projects_summary.append(project)
 	return render_template('projects/index.html', projects_summary=projects_summary)
-
+@projects.route('/<projectname>')
+def projectread(projectname):
+	project_detail = query_db("select * from projects where subject=?", [projectname])
+	project_detail = project_detail[0]
+	if project_detail == []:
+		return "Its a 404 buddy",404
+	return render_template("projects/projdet.html", project_detail = project_detail)
 
