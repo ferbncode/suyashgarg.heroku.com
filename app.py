0b8d62d63d67edffe7435aef9662478a88f9da62
ferbncode <suyashgargsfam@gmail.com>
2016-07-14 01:36:54 +0545

    I am fed up

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+
 from mysite import app
-app.run(debug=True, host='0.0.0.0', port=5000)
+
+if __name__ == '__main__':
+	app.run(debug=True, host='0.0.0.0')
 
--- mysite/__init__.py
+++ mysite/__init__.py
@@ -36,4 +36,3 @@ def resume():
 
 
 
-
